ee3a0e482ced732b6216652c140809bc30e79797
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-11 13:47:22 -0700

    some structuring changes to html, add id's for js targeting, notes in js for quick ref to my targeting

---
+++ Script.js
@@ -0,0 +1,43 @@
+//08135b237911526a5b44b4a483a8142a api key from openweather
+const myAPIKey = "08135b237911526a5b44b4a483a8142a";
+
+// id's to be used later for targeting:
+// search and history stuff
+// id="search-weather"
+// id="search-history"
+// id="clear-history"
+// jumbotron current
+// id="currentcon-city"
+// id="currentcon-icon"
+// id="currentcon-temp"
+// id="currentcon-humidity"
+// id="currentcon-wind"
+// id="currentcon-uvi"
+// forcast 5 day
+// id="forcast-date-1"
+// id="forcast-temp-1"
+// id="forcast-humidity-1"
+// id="forcast-wind-1"
+// id="forcast-uvi-1"
+// id="forcast-date-2"
+// id="forcast-temp-2"
+// id="forcast-humidity-2"
+// id="forcast-wind-2"
+// id="forcast-uvi-2"
+// id="forcast-date-3"
+// id="forcast-temp-3"
+// id="forcast-humidity-3"
+// id="forcast-wind-3"
+// id="forcast-uvi-3"
+// id="forcast-date-4"
+// id="forcast-temp-4"
+// id="forcast-humidity-4"
+// id="forcast-wind-4"
+// id="forcast-uvi-4"
+// id="forcast-date-5"
+// id="forcast-temp-5"
+// id="forcast-humidity-5"
+// id="forcast-wind-5"
+// id="forcast-uvi-5"
+
+// starting variables
