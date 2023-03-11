77e9a75793eb2ad314ae1be426d0cf65fe85d579
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-11 14:41:46 -0700

    starting variables

--- Script.js
+++ Script.js
@@ -1,19 +1,20 @@
 //08135b237911526a5b44b4a483a8142a api key from openweather
 const myAPIKey = "08135b237911526a5b44b4a483a8142a";
 
+// url construstion : http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
 // id's to be used later for targeting:
-// search and history stuff
-// id="search-weather"
+// ***search and history stuff
 // id="search-history"
-// id="clear-history"
-// jumbotron current
+// id="clear-history-btn"
+// ***jumbotron current
 // id="currentcon-city"
 // id="currentcon-icon"
 // id="currentcon-temp"
 // id="currentcon-humidity"
 // id="currentcon-wind"
 // id="currentcon-uvi"
-// forcast 5 day
+// ***forcast 5 day
 // id="forcast-date-1"
 // id="forcast-temp-1"
 // id="forcast-humidity-1"
@@ -41,3 +42,19 @@ const myAPIKey = "08135b237911526a5b44b4a483a8142a";
 // id="forcast-uvi-5"
 
 // starting variables
+// id="search-city-btn"
+const searchButton = document.getElementById("search-city-btn");
+// id="search-city-input"
+var cityInput = document.getElementById("search-city-input");
+
+var openweatherURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=3&appid=${myAPIKey}`
+
+button.addEventListener('click', () => {
+    fetch(openweatherURL)
+        .then(function (result) {
+            console.log(result);
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+});
